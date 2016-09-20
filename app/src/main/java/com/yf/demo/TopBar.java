63c665504eb75876de8ab12ae5c914b1f76eca3d
无锡 <536818@qq.com>
2016-09-20 15:33:11 +0800

    add topbar.java

---
+++ app/src/main/java/com/yf/demo/TopBar.java
@@ -0,0 +1,21 @@
+package com.yf.demo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by wuyifeng on 2016/9/20.
+ */
+
+public class TopBar extends ViewGroup {
+
+    public TopBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
